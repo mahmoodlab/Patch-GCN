bb8596c9e9dd4fbf51758a943c87a178bf376b68
miccai2021anon <miccai2021anon@gmail.com>
2021-05-21 15:47:11 -0400

    Adding splits + models + utils + data

---
+++ models/model_graph_mil.py
@@ -0,0 +1,238 @@
+from os.path import join
+from collections import OrderedDict
+
+import pdb
+import numpy as np
+
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+from torch.nn import Sequential as Seq
+from torch.nn import Linear, LayerNorm, ReLU
+from torch_geometric.nn import GCNConv, GraphConv, GatedGraphConv, GATConv, SGConv, GINConv, GENConv, DeepGCNLayer
+from torch_geometric.nn import GraphConv, TopKPooling, SAGPooling
+from torch_geometric.nn import global_mean_pool as gavgp, global_max_pool as gmp, global_add_pool as gap
+from torch_geometric.transforms.normalize_features import NormalizeFeatures
+
+
+"""
+Attention Network without Gating (2 fc layers)
+args:
+    L: input feature dimension
+    D: hidden layer dimension
+    dropout: whether to use dropout (p = 0.25)
+    n_classes: number of classes (experimental usage for multiclass MIL)
+"""
+class Attn_Net(nn.Module):
+
+    def __init__(self, L = 1024, D = 256, dropout = False, n_classes = 1):
+        super(Attn_Net, self).__init__()
+        self.module = [
+            nn.Linear(L, D),
+            nn.Tanh()]
+
+        if dropout:
+            self.module.append(nn.Dropout(0.25))
+
+        self.module.append(nn.Linear(D, n_classes))
+        
+        self.module = nn.Sequential(*self.module)
+    
+    def forward(self, x):
+        return self.module(x), x # N x n_classes
+
+"""
+
+Attention Network with Sigmoid Gating (3 fc layers)
+args:
+    L: input feature dimension
+    D: hidden layer dimension
+    dropout: whether to use dropout (p = 0.25)
+    n_classes: number of classes (experimental usage for multiclass MIL)
+"""
+class Attn_Net_Gated(nn.Module):
+
+    def __init__(self, L = 1024, D = 256, dropout = False, n_classes = 1):
+        super(Attn_Net_Gated, self).__init__()
+        self.attention_a = [
+            nn.Linear(L, D),
+            nn.Tanh()]
+        
+        self.attention_b = [nn.Linear(L, D),
+                            nn.Sigmoid()]
+        if dropout:
+            self.attention_a.append(nn.Dropout(0.25))
+            self.attention_b.append(nn.Dropout(0.25))
+
+        self.attention_a = nn.Sequential(*self.attention_a)
+        self.attention_b = nn.Sequential(*self.attention_b)
+        
+        self.attention_c = nn.Linear(D, n_classes)
+
+    def forward(self, x):
+        a = self.attention_a(x)
+        b = self.attention_b(x)
+        A = a.mul(b)
+        A = self.attention_c(A)  # N x n_classes
+        return A, x
+
+class NormalizeFeaturesV2(object):
+    r"""Column-normalizes node features to sum-up to one."""
+
+    def __call__(self, data):
+        data.x[:, :12] = data.x[:, :12] / data.x[:, :12].max(0, keepdim=True)[0]
+        return data
+
+    def __repr__(self):
+        return '{}()'.format(self.__class__.__name__)
+
+class NormalizeEdgesV2(object):
+    r"""Column-normalizes node features to sum-up to one."""
+
+    def __call__(self, data):
+        data.edge_attr = data.edge_attr.type(torch.cuda.FloatTensor)
+        data.edge_attr = data.edge_attr / data.edge_attr.max(0, keepdim=True)[0]
+        return data
+
+    def __repr__(self):
+        return '{}()'.format(self.__class__.__name__)
+
+class DeepGraphConv_Surv(torch.nn.Module):
+    def __init__(self, edge_agg='latent', resample=0, num_features=1024, hidden_dim=256, 
+        linear_dim=256, use_edges=False, dropout=0.25, n_classes=4):
+        super(DeepGraphConv_Surv, self).__init__()
+        self.use_edges = use_edges
+        self.size_dict_path = {"small": [1024, 512, 256], "big": [1024, 512, 384]}
+        self.resample = resample
+        self.edge_agg = edge_agg
+
+        if self.resample > 0:
+            self.fc = nn.Sequential(*[nn.Dropout(self.resample)])
+
+        self.conv1 = GINConv(Seq(nn.Linear(num_features, hidden_dim), nn.ReLU(), nn.Linear(hidden_dim, hidden_dim)))
+        self.conv2 = GINConv(Seq(nn.Linear(hidden_dim, hidden_dim), nn.ReLU(), nn.Linear(hidden_dim, hidden_dim)))
+        self.conv3 = GINConv(Seq(nn.Linear(hidden_dim, hidden_dim), nn.ReLU(), nn.Linear(hidden_dim, hidden_dim)))
+        
+        self.path_attention_head = Attn_Net_Gated(L=hidden_dim, D=hidden_dim, dropout=dropout, n_classes=1)
+        self.path_rho = nn.Sequential(*[nn.Linear(hidden_dim, hidden_dim), nn.ReLU(), nn.Dropout(dropout)])
+        self.classifier = torch.nn.Linear(hidden_dim, n_classes)
+
+    def relocate(self):
+        from torch_geometric.nn import DataParallel
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        if torch.cuda.device_count() >= 1:
+            device_ids = list(range(torch.cuda.device_count()))
+            self.conv1 = nn.DataParallel(self.conv1, device_ids=device_ids).to('cuda:0')
+            self.conv2 = nn.DataParallel(self.conv2, device_ids=device_ids).to('cuda:0')
+            self.conv3 = nn.DataParallel(self.conv3, device_ids=device_ids).to('cuda:0')
+            self.path_attention_head = nn.DataParallel(self.path_attention_head, device_ids=device_ids).to('cuda:0')
+
+        self.path_rho = self.path_rho.to(device)
+        self.classifier = self.classifier.to(device)
+
+    def forward(self, **kwargs):
+        data = kwargs['x_path']
+        x = data.x
+        if self.edge_agg == 'spatial':
+            edge_index = data.edge_index
+        elif self.edge_agg == 'latent':
+            edge_index = data.edge_latent
+        batch = data.batch
+        edge_attr = None
+
+        if self.resample:
+            x = self.fc(x)
+
+        x1 = F.relu(self.conv1(x, edge_index, edge_attr))
+        if self.pool:
+            x1, edge_index, _, batch, perm, score = self.pool1(x1, edge_index, None, batch)
+            x1_cat = torch.cat([gmp(x1, batch), gap(x1, batch)], dim=1)
+
+        x2 = F.relu(self.conv2(x1, edge_index, edge_attr))
+        if self.pool:
+            x2, edge_index, _, batch, perm, score = self.pool2(x2, edge_index, None, batch)
+            x2_cat = torch.cat([gmp(x2, batch), gap(x2, batch)], dim=1)
+
+        x3 = F.relu(self.conv3(x2, edge_index, edge_attr))
+        
+
+        h_path = x3
+
+        A_path, h_path = self.path_attention_head(h_path)
+        A_path = torch.transpose(A_path, 1, 0)
+        h_path = torch.mm(F.softmax(A_path, dim=1) , h_path)
+        h_path = self.path_rho(h_path).squeeze()
+        h = h_path # [256] vector
+        
+        logits  = self.classifier(h).unsqueeze(0) # logits needs to be a [1 x 4] vector 
+        Y_hat = torch.topk(logits, 1, dim = 1)[1]
+        hazards = torch.sigmoid(logits)
+        S = torch.cumprod(1 - hazards, dim=1)
+        
+        return hazards, S, Y_hat, A_path, None
+
+
+class PatchGCN_Surv(torch.nn.Module):
+    def __init__(self, num_layers=4, edge_agg='spatial', resample=0,
+        num_features=1024, hidden_dim=128, linear_dim=64, use_edges=False, dropout=0.25, n_classes=4):
+        super(PatchGCN_Surv, self).__init__()
+        self.use_edges = use_edges
+        self.size_dict_path = {"small": [1024, 512, 256], "big": [1024, 512, 384]}
+        self.edge_agg = edge_agg
+        self.num_layers = num_layers-1
+        self.resample = resample
+
+        if self.resample > 0:
+            self.fc = nn.Sequential(*[nn.Dropout(self.resample), nn.Linear(1024, 256), nn.ReLU(), nn.Dropout(0.25)])
+        else:
+            self.fc = nn.Sequential(*[nn.Linear(1024, 128), nn.ReLU(), nn.Dropout(0.25)])
+
+        self.layers = torch.nn.ModuleList()
+        for i in range(1, self.num_layers+1):
+            conv = GENConv(hidden_dim, hidden_dim, aggr='softmax',
+                           t=1.0, learn_t=True, num_layers=2, norm='layer')
+            norm = LayerNorm(hidden_dim, elementwise_affine=True)
+            act = ReLU(inplace=True)
+            layer = DeepGCNLayer(conv, norm, act, block='res', dropout=0.1, ckpt_grad=i % 3)
+            self.layers.append(layer)
+
+        self.path_phi = nn.Sequential(*[nn.Linear(hidden_dim*4, hidden_dim*4), nn.ReLU(), nn.Dropout(0.25)])
+        self.path_attention_head = Attn_Net_Gated(L=hidden_dim*4, D=hidden_dim*4, dropout=dropout, n_classes=1)
+        self.path_rho = nn.Sequential(*[nn.Linear(hidden_dim*4, hidden_dim*4), nn.ReLU(), nn.Dropout(dropout)])
+        self.classifier = torch.nn.Linear(hidden_dim*4, n_classes)    
+
+    def forward(self,  **kwargs):
+        data = kwargs['x_path']
+                
+        if self.edge_agg == 'spatial':
+            edge_index = data.edge_index
+        elif self.edge_agg == 'latent':
+            edge_index = data.edge_latent
+
+        batch = data.batch
+        edge_attr = None
+
+        x = self.fc(data.x)
+        x_gin = x # F.relu(self.conv(x, edge_index, edge_attr))
+
+        x = self.layers[0].conv(x_gin, edge_index, edge_attr)
+        x_gin = torch.cat([x_gin, x], axis=1)
+        for layer in self.layers[1:]:
+            x = layer(x, edge_index, edge_attr)
+            x_gin = torch.cat([x_gin, x], axis=1)
+        
+        h_path = x_gin
+        h_path = self.path_phi(h_path)
+
+        A_path, h_path = self.path_attention_head(h_path)
+        A_path = torch.transpose(A_path, 1, 0)
+        h_path = torch.mm(F.softmax(A_path, dim=1), h_path)
+        h_path = self.path_rho(h_path).squeeze()
+        h = h_path # [256] vector
+            
+        logits  = self.classifier(h).unsqueeze(0) # logits needs to be a [1 x 4] vector 
+        Y_hat = torch.topk(logits, 1, dim = 1)[1]
+        hazards = torch.sigmoid(logits)
+        S = torch.cumprod(1 - hazards, dim=1)
+        
+        return hazards, S, Y_hat, A_path, None
---
+++ utils/utils.py
@@ -0,0 +1,357 @@
+import pickle
+import torch
+import numpy as np
+import torch.nn as nn
+import pdb
+
+import torch
+import numpy as np
+import torch.nn as nn
+from torchvision import transforms
+from torch.utils.data import DataLoader, Sampler, WeightedRandomSampler, RandomSampler, SequentialSampler, sampler
+import torch.optim as optim
+import pdb
+import torch.nn.functional as F
+import math
+from itertools import islice
+import collections
+
+from torch.utils.data.dataloader import default_collate
+import torch_geometric
+from torch_geometric.data import Batch
+
+device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class SubsetSequentialSampler(Sampler):
+    """Samples elements sequentially from a given list of indices, without replacement.
+
+    Arguments:
+        indices (sequence): a sequence of indices
+    """
+    def __init__(self, indices):
+        self.indices = indices
+
+    def __iter__(self):
+        return iter(self.indices)
+
+    def __len__(self):
+        return len(self.indices)
+
+def collate_MIL(batch):
+    img = torch.cat([item[0] for item in batch], dim = 0)
+    label = torch.LongTensor([item[1] for item in batch])
+    return [img, label]
+
+def collate_features(batch):
+    img = torch.cat([item[0] for item in batch], dim = 0)
+    coords = np.vstack([item[1] for item in batch])
+    return [img, coords]
+
+def collate_MIL_survival(batch):
+    img = torch.cat([item[0] for item in batch], dim = 0)
+    meta = torch.cat([item[1] for item in batch], dim = 0).type(torch.FloatTensor)
+    label = torch.LongTensor([item[2] for item in batch])
+    event_time = np.array([item[3] for item in batch])
+    c = torch.FloatTensor([item[4] for item in batch])
+    return [img, meta, label, event_time, c]
+
+def collate_MIL_survival_cluster(batch):
+    img = torch.cat([item[0] for item in batch], dim = 0)
+    cluster_ids = torch.cat([item[1] for item in batch], dim = 0).type(torch.LongTensor)
+    meta = torch.cat([item[2] for item in batch], dim = 0).type(torch.FloatTensor)
+    label = torch.LongTensor([item[3] for item in batch])
+    event_time = np.array([item[4] for item in batch])
+    c = torch.FloatTensor([item[5] for item in batch])
+    return [img, cluster_ids, meta, label, event_time, c]
+
+def collate_MIL_survival_graph(batch):
+    elem = batch[0]
+    elem_type = type(elem)        
+    transposed = zip(*batch)
+    return [samples[0] if isinstance(samples[0], torch_geometric.data.Batch) else default_collate(samples) for samples in transposed]
+
+
+
+def get_simple_loader(dataset, batch_size=1):
+    kwargs = {'num_workers': 4} if device.type == "cuda" else {}
+    loader = DataLoader(dataset, batch_size=batch_size, sampler = sampler.SequentialSampler(dataset), collate_fn = collate_MIL, **kwargs)
+    return loader 
+
+def get_split_loader(split_dataset, training = False, testing = False, weighted = False, mode='coattn', batch_size=1):
+    """
+        return either the validation loader or training loader 
+    """
+    if mode == 'graph':
+        collate = collate_MIL_survival_graph
+    elif mode == 'cluster':
+        collate = collate_MIL_survival_cluster
+    else:
+        collate = collate_MIL_survival
+
+    kwargs = {'num_workers': 4} if device.type == "cuda" else {}
+    if not testing:
+        if training:
+            if weighted:
+                weights = make_weights_for_balanced_classes_split(split_dataset)
+                loader = DataLoader(split_dataset, batch_size=batch_size, sampler = WeightedRandomSampler(weights, len(weights)), collate_fn = collate, **kwargs)    
+            else:
+                loader = DataLoader(split_dataset, batch_size=batch_size, sampler = RandomSampler(split_dataset), collate_fn = collate, **kwargs)
+        else:
+            loader = DataLoader(split_dataset, batch_size=batch_size, sampler = SequentialSampler(split_dataset), collate_fn = collate, **kwargs)
+    
+    else:
+        ids = np.random.choice(np.arange(len(split_dataset), int(len(split_dataset)*0.1)), replace = False)
+        loader = DataLoader(split_dataset, batch_size=1, sampler = SubsetSequentialSampler(ids), collate_fn = collate, **kwargs )
+
+    return loader
+
+def get_optim(model, args):
+    if args.opt == "adam":
+        optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=args.lr, weight_decay=args.reg)
+    elif args.opt == 'sgd':
+        optimizer = optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=args.lr, momentum=0.9, weight_decay=args.reg)
+    else:
+        raise NotImplementedError
+    return optimizer
+
+def print_network(net):
+    num_params = 0
+    num_params_train = 0
+    print(net)
+    
+    for param in net.parameters():
+        n = param.numel()
+        num_params += n
+        if param.requires_grad:
+            num_params_train += n
+    
+    print('Total number of parameters: %d' % num_params)
+    print('Total number of trainable parameters: %d' % num_params_train)
+
+
+def generate_split(cls_ids, val_num, test_num, samples, n_splits = 5,
+    seed = 7, label_frac = 1.0, custom_test_ids = None):
+    indices = np.arange(samples).astype(int)
+    
+    pdb.set_trace()
+    if custom_test_ids is not None:
+        indices = np.setdiff1d(indices, custom_test_ids)
+
+    np.random.seed(seed)
+    for i in range(n_splits):
+        all_val_ids = []
+        all_test_ids = []
+        sampled_train_ids = []
+        
+        if custom_test_ids is not None: # pre-built test split, do not need to sample
+            all_test_ids.extend(custom_test_ids)
+
+        for c in range(len(val_num)):
+            possible_indices = np.intersect1d(cls_ids[c], indices) #all indices of this class
+            remaining_ids = possible_indices
+
+            if val_num[c] > 0:
+                val_ids = np.random.choice(possible_indices, val_num[c], replace = False) # validation ids
+                remaining_ids = np.setdiff1d(possible_indices, val_ids) #indices of this class left after validation
+                all_val_ids.extend(val_ids)
+
+            if custom_test_ids is None and test_num[c] > 0: # sample test split
+
+                test_ids = np.random.choice(remaining_ids, test_num[c], replace = False)
+                remaining_ids = np.setdiff1d(remaining_ids, test_ids)
+                all_test_ids.extend(test_ids)
+
+            if label_frac == 1:
+                sampled_train_ids.extend(remaining_ids)
+            
+            else:
+                sample_num  = math.ceil(len(remaining_ids) * label_frac)
+                slice_ids = np.arange(sample_num)
+                sampled_train_ids.extend(remaining_ids[slice_ids])
+
+        yield sorted(sampled_train_ids), sorted(all_val_ids), sorted(all_test_ids)
+
+
+def nth(iterator, n, default=None):
+    if n is None:
+        return collections.deque(iterator, maxlen=0)
+    else:
+        return next(islice(iterator,n, None), default)
+
+def calculate_error(Y_hat, Y):
+    error = 1. - Y_hat.float().eq(Y.float()).float().mean().item()
+
+    return error
+
+def make_weights_for_balanced_classes_split(dataset):
+    N = float(len(dataset))                                           
+    weight_per_class = [N/len(dataset.slide_cls_ids[c]) for c in range(len(dataset.slide_cls_ids))]                                                                                                     
+    weight = [0] * int(N)                                           
+    for idx in range(len(dataset)):   
+        y = dataset.getlabel(idx)                        
+        weight[idx] = weight_per_class[y]                                  
+
+    return torch.DoubleTensor(weight)
+
+def initialize_weights(module):
+    for m in module.modules():
+        if isinstance(m, nn.Linear):
+            nn.init.xavier_normal_(m.weight)
+            m.bias.data.zero_()
+        
+        elif isinstance(m, nn.BatchNorm1d):
+            nn.init.constant_(m.weight, 1)
+            nn.init.constant_(m.bias, 0)
+
+
+def dfs_freeze(model):
+    for name, child in model.named_children():
+        for param in child.parameters():
+            param.requires_grad = False
+        dfs_freeze(child)
+
+
+def dfs_unfreeze(model):
+    for name, child in model.named_children():
+        for param in child.parameters():
+            param.requires_grad = True
+        dfs_unfreeze(child)
+
+def nll_loss(hazards, S, Y, c, alpha=0.4, eps=1e-7):
+    batch_size = len(Y)
+    Y = Y.view(batch_size, 1) # ground truth bin, 1,2,...,k
+    c = c.view(batch_size, 1).float() #censorship status, 0 or 1
+    if S is None:
+        S = torch.cumprod(1 - hazards, dim=1) # surival is cumulative product of 1 - hazards
+    # without padding, S(0) = S[0], h(0) = h[0]
+    S_padded = torch.cat([torch.ones_like(c), S], 1) #S(-1) = 0, all patients are alive from (-inf, 0) by definition
+    # after padding, S(0) = S[1], S(1) = S[2], etc, h(0) = h[0]
+    #h[y] = h(1)
+    #S[1] = S(1)
+    uncensored_loss = -(1 - c) * (torch.log(torch.gather(S_padded, 1, Y).clamp(min=eps)) + torch.log(torch.gather(hazards, 1, Y).clamp(min=eps)))
+    censored_loss = - c * torch.log(torch.gather(S_padded, 1, Y+1).clamp(min=eps))
+    neg_l = censored_loss + uncensored_loss
+    loss = (1-alpha) * neg_l + alpha * uncensored_loss
+    loss = loss.mean()
+    return loss
+
+def ce_loss(hazards, S, Y, c, alpha=0.4, eps=1e-7):
+    batch_size = len(Y)
+    Y = Y.view(batch_size, 1) # ground truth bin, 1,2,...,k
+    c = c.view(batch_size, 1).float() #censorship status, 0 or 1
+    if S is None:
+        S = torch.cumprod(1 - hazards, dim=1) # surival is cumulative product of 1 - hazards
+    # without padding, S(0) = S[0], h(0) = h[0]
+    # after padding, S(0) = S[1], S(1) = S[2], etc, h(0) = h[0]
+    #h[y] = h(1)
+    #S[1] = S(1)
+    S_padded = torch.cat([torch.ones_like(c), S], 1)
+    reg = -(1 - c) * (torch.log(torch.gather(S_padded, 1, Y)+eps) + torch.log(torch.gather(hazards, 1, Y).clamp(min=eps)))
+    ce_l = - c * torch.log(torch.gather(S, 1, Y).clamp(min=eps)) - (1 - c) * torch.log(1 - torch.gather(S, 1, Y).clamp(min=eps))
+    loss = (1-alpha) * ce_l + alpha * reg
+    loss = loss.mean()
+    return loss
+
+class CrossEntropySurvLoss(object):
+    def __init__(self, alpha=0.15):
+        self.alpha = alpha
+
+    def __call__(self, hazards, S, Y, c, alpha=None): 
+        if alpha is None:
+            return ce_loss(hazards, S, Y, c, alpha=self.alpha)
+        else:
+            return ce_loss(hazards, S, Y, c, alpha=alpha)
+
+# loss_fn(hazards=hazards, S=S, Y=Y_hat, c=c, alpha=0)
+class NLLSurvLoss(object):
+    def __init__(self, alpha=0.15):
+        self.alpha = alpha
+
+    def __call__(self, hazards, S, Y, c, alpha=None):
+        if alpha is None:
+            return nll_loss(hazards, S, Y, c, alpha=self.alpha)
+        else:
+            return nll_loss(hazards, S, Y, c, alpha=alpha)
+    # h_padded = torch.cat([torch.zeros_like(c), hazards], 1)
+    #reg = - (1 - c) * (torch.log(torch.gather(hazards, 1, Y)) + torch.gather(torch.cumsum(torch.log(1-h_padded), dim=1), 1, Y))
+
+
+class CoxSurvLoss(object):
+    def __call__(hazards, S, c, **kwargs):
+        # This calculation credit to Travers Ching https://github.com/traversc/cox-nnet
+        # Cox-nnet: An artificial neural network method for prognosis prediction of high-throughput omics data
+        current_batch_len = len(S)
+        R_mat = np.zeros([current_batch_len, current_batch_len], dtype=int)
+        for i in range(current_batch_len):
+            for j in range(current_batch_len):
+                R_mat[i,j] = S[j] >= S[i]
+
+        R_mat = torch.FloatTensor(R_mat).to(device)
+        theta = hazards.reshape(-1)
+        exp_theta = torch.exp(theta)
+        loss_cox = -torch.mean((theta - torch.log(torch.sum(exp_theta*R_mat, dim=1))) * (1-c))
+        return loss_cox
+
+def l1_reg_all(model, reg_type=None):
+    l1_reg = None
+
+    for W in model.parameters():
+        if l1_reg is None:
+            l1_reg = torch.abs(W).sum()
+        else:
+            l1_reg = l1_reg + torch.abs(W).sum() # torch.abs(W).sum() is equivalent to W.norm(1)
+    return l1_reg
+
+def get_custom_exp_code(args):
+    ### New exp_code
+    exp_code = '_'.join(args.split_dir.split('_')[:2])
+    dataset_path = 'dataset_csv'
+    param_code = ''
+
+    if args.model_type == 'amil':
+        param_code += 'AMIL'
+    elif args.model_type == 'deepset':
+        param_code += 'DS'
+    elif args.model_type == 'mi_fcn':
+        param_code += 'MIFCN'
+    elif args.model_type == 'dgc':
+        agg = 'latent' if args.edge_agg == 'latent' else 'spatial'
+        param_code += 'DGC_%s%s' % (agg)
+    elif args.model_type == 'patchgcn':
+        param_code += 'PatchGCN'
+    else:
+        raise NotImplementedError
+
+    if 'small' in args.model_size_wsi:
+        param_code += 'sm'
+
+    if args.resample > 0:
+        param_code += '_resample'
+
+    param_code += '_%s' % args.bag_loss
+
+    param_code += '_a%s' % str(args.alpha_surv)
+
+    if args.lr != 2e-4:
+        param_code += '_lr%s' % format(args.lr, '.0e')
+
+    if args.reg_type != 'None':
+        param_code += '_reg%s' % format(args.lambda_reg, '.0e')
+
+    param_code += '_%s' % args.which_splits.split("_")[0]
+
+    if args.batch_size != 1:
+        param_code += '_b%s' % str(args.batch_size)
+
+    if args.gc != 1:
+        param_code += '_gc%s' % str(args.gc)
+
+    if args.apply_sigfeats:
+        param_code += '_sig'
+        dataset_path += '_sig'
+
+    args.exp_code = exp_code + '_' + param_code
+    args.param_code = param_code
+    args.dataset_path = dataset_path
+
+    return args
