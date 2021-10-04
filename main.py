0f9e94152efc0c13e1b3f06279ffe21445a7f2da
Richard Chen <richen1939@gmail.com>
2021-10-04 01:01:11 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,254 @@
+from __future__ import print_function
+
+import argparse
+import pdb
+import os
+import math
+import sys
+
+# internal imports
+from utils.file_utils import save_pkl, load_pkl
+from utils.utils import *
+from utils.core_utils import train
+from datasets.dataset_generic import Generic_WSI_Classification_Dataset, Generic_MIL_Dataset
+from datasets.dataset_survival import Generic_WSI_Survival_Dataset, Generic_MIL_Survival_Dataset
+
+# pytorch imports
+import torch
+from torch.utils.data import DataLoader, sampler
+import torch.nn as nn
+import torch.nn.functional as F
+
+import pandas as pd
+import numpy as np
+
+from timeit import default_timer as timer
+
+  
+def main(args):
+    # create results directory if necessary
+    if not os.path.isdir(args.results_dir):
+        os.mkdir(args.results_dir)
+
+    if args.k_start == -1:
+        start = 0
+    else:
+        start = args.k_start
+    if args.k_end == -1:
+        end = args.k
+    else:
+        end = args.k_end
+
+    latest_val_cindex = []
+    folds = np.arange(start, end)
+
+    for i in folds:
+
+        start = timer()
+        seed_torch(args.seed)
+        
+        results_pkl_path = os.path.join(args.results_dir, 'split_latest_val_{}_results.pkl'.format(i))
+        if os.path.isfile(results_pkl_path):
+            print("Skipping Split %d" % i)
+            continue
+
+        train_dataset, val_dataset = dataset.return_splits(from_id=False, 
+                csv_path='{}/splits_{}.csv'.format(args.split_dir, i))
+        
+        print('training: {}, validation: {}'.format(len(train_dataset), len(val_dataset)))
+        datasets = (train_dataset, val_dataset)
+        
+        if args.task_type == 'survival':
+            results = train(datasets, i, args)
+            val_latest, cindex_latest = results['latest']
+            latest_val_cindex.append(cindex_latest)
+
+        #write results to pkl
+        save_pkl(results_pkl_path, val_latest)
+
+        end = timer()
+        print('Fold %d Time: %f seconds' % (i, end - start))
+
+
+    if args.task_type == 'survival':
+        results_latest_df = pd.DataFrame({'folds': folds, 'val_cindex': latest_val_cindex})
+
+    if len(folds) != args.k:
+        save_name = 'summary_partial_{}_{}.csv'.format(start, end)
+    else:
+        save_name = 'summary.csv'
+
+    results_latest_df.to_csv(os.path.join(args.results_dir, 'summary_latest.csv'))
+
+# Training settings
+parser = argparse.ArgumentParser(description='Configurations for WSI Training')
+parser.add_argument('--data_root_dir', type=str, default='/media/ssd1/pan-cancer/', 
+                    help='data directory')
+
+parser.add_argument('--mode', type=str, default='omic')
+parser.add_argument('--apply_mad', action='store_true', default=False)
+parser.add_argument('--apply_sig', action='store_true', default=False)
+parser.add_argument('--apply_mutsig', action='store_true', default=False)
+parser.add_argument('--apply_sim', action='store_true', default=False)
+parser.add_argument('--num_gcn_layers', type=int, default=4)
+parser.add_argument('--edge_agg', type=str, default='spatial')
+parser.add_argument('--multires', action='store_true', default=False)
+parser.add_argument('--depth', type=int, default=2)
+
+parser.add_argument('--max_epochs', type=int, default=20,
+                    help='maximum number of epochs to train (default: 20)')
+parser.add_argument('--lr', type=float, default=2e-4,
+                    help='learning rate (default: 0.0001)')
+parser.add_argument('--label_frac', type=float, default=1.0,
+                    help='fraction of training labels (default: 1.0)')
+parser.add_argument('--bag_weight', type=float, default=0.7,
+                    help='clam: weight coefficient for bag-level loss (default: 0.7)')
+parser.add_argument('--reg', type=float, default=1e-5,
+                    help='weight decay (default: 1e-5)')
+parser.add_argument('--seed', type=int, default=1, 
+                    help='random seed for reproducible experiment (default: 1)')
+parser.add_argument('--k', type=int, default=5, help='number of folds (default: 10)')
+parser.add_argument('--k_start', type=int, default=-1, help='start fold (default: -1, last fold)')
+parser.add_argument('--k_end', type=int, default=-1, help='end fold (default: -1, first fold)')
+parser.add_argument('--results_dir', default='./results_rp', help='results directory (default: ./results)')
+parser.add_argument('--split_dir', type=str, default=None, 
+                    help='manually specify the set of splits to use, ' 
+                    +'instead of infering from the task and label_frac argument (default: None)')
+parser.add_argument('--which_splits', type=str, default='mixed_testing')
+parser.add_argument('--log_data', action='store_true', default=True, help='log data using tensorboard')
+parser.add_argument('--opt', type=str, choices = ['adam', 'sgd'], default='adam')
+parser.add_argument('--drop_out', action='store_true', default=True, help='enabel dropout (p=0.25)')
+parser.add_argument('--alpha_surv', type=float, default=0.0, help='How much to weigh uncensored patients')
+parser.add_argument('--model_type', type=str, default='clam', help='type of model (default: clam)')
+parser.add_argument('--reg_type', type=str, choices=['None', 'omic', 'pathomic'], default='None', help='Reg Type (default: None)')
+parser.add_argument('--lambda_reg', type=float, default=1e-5, help='Regularization Strength')
+parser.add_argument('--exp_code', type=str, default=None, help='Experiment code for saving results')
+parser.add_argument('--weighted_sample', action='store_true', default=True, help='enable weighted sampling')
+parser.add_argument('--task', type=str, default='NA')
+parser.add_argument('--gc', type=int, default=32, help='gradient accumulation step')
+parser.add_argument('--batch_size', type=int, default=1, help='batch_size')
+parser.add_argument('--resample', type=float, default=0.00, help='batch_size')
+
+args = parser.parse_args()
+device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+### Experiment Code
+if args.exp_code == None:
+  args = get_custom_exp_code(args)
+
+### Task
+if args.task == 'NA':
+  args.task = '_'.join(args.split_dir.split('_')[:2]) + '_survival'
+print("Experiment Name:", args.exp_code)
+
+def seed_torch(seed=7):
+    import random
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if device.type == 'cuda':
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed) # if you are using multi-GPU.
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+seed_torch(args.seed)
+
+encoding_size = 1024
+settings = {'num_splits': args.k, 
+            'k_start': args.k_start,
+            'k_end': args.k_end,
+            'task': args.task,
+            'max_epochs': args.max_epochs, 
+            'results_dir': args.results_dir, 
+            'lr': args.lr,
+            'experiment': args.exp_code,
+            'reg': args.reg,
+            'label_frac': args.label_frac,
+            'inst_loss': args.inst_loss,
+            'bag_loss': args.bag_loss,
+            'bag_weight': args.bag_weight,
+            'seed': args.seed,
+            'model_type': args.model_type,
+            'model_size_wsi': args.model_size_wsi,
+            'model_size_omic': args.model_size_omic,
+            "use_drop_out": args.drop_out,
+            'weighted_sample': args.weighted_sample,
+            'gc': args.gc,
+            'opt': args.opt}
+
+print('\nLoad Dataset')
+
+if 'survival' in args.task:
+  args.n_classes = 4
+  study = '_'.join(args.task.split('_')[:2])
+  if study == 'tcga_kirc' or study == 'tcga_kirp':
+    combined_study = 'tcga_kidney'
+  elif study == 'tcga_luad' or study == 'tcga_lusc':
+    combined_study = 'tcga_lung'
+  else:
+    combined_study = study
+
+  dataset = Generic_MIL_Survival_Dataset(csv_path = './%s/%s_all_clean.csv.zip' % (args.dataset_path, combined_study),
+                                           mode = args.mode,
+                                           ignore_rnaseq = args.ignore_rnaseq,
+                                           pca_comp = args.pca_comp,
+                                           apply_sig = args.apply_sig,
+                                           data_dir= os.path.join(args.data_root_dir, study_dir),
+                                           shuffle = False, 
+                                           seed = args.seed, 
+                                           print_info = True,
+                                           patient_strat= False,
+                                           n_bins=4,
+                                           label_col = 'survival_months',
+                                           ignore=[])
+else:
+  raise NotImplementedError
+
+if isinstance(dataset, Generic_MIL_Survival_Dataset):
+    args.task_type ='survival'
+else:
+    raise NotImplementedError
+
+if not os.path.isdir(args.results_dir):
+    os.mkdir(args.results_dir)
+
+args.results_dir = os.path.join(args.results_dir, args.which_splits, args.param_code, str(args.exp_code) + '_s{}'.format(args.seed))
+if not os.path.isdir(args.results_dir):
+    os.makedirs(args.results_dir)
+
+if ('summary_latest.csv' in os.listdir(args.results_dir)) and (not args.overwrite):
+  print("Exp Code <%s> already exists! Exiting script." % args.exp_code)
+  sys.exit()
+
+if args.split_dir is None:
+    args.split_dir = os.path.join('./splits', args.task+'_{}'.format(int(args.label_frac*100)))
+else:
+    args.split_dir = os.path.join('./splits', args.which_splits, args.split_dir)
+
+print("split_dir", args.split_dir)
+
+assert os.path.isdir(args.split_dir)
+
+settings.update({'split_dir': args.split_dir})
+
+
+with open(args.results_dir + '/experiment_{}.txt'.format(args.exp_code), 'w') as f:
+    print(settings, file=f)
+f.close()
+
+print("################# Settings ###################")
+for key, val in settings.items():
+    print("{}:  {}".format(key, val))        
+
+if __name__ == "__main__":
+
+    start = timer()
+    results = main(args)
+    end = timer()
+    print("finished!")
+    print("end script")
+    print('Script Time: %f seconds' % (end - start))
+
+
